fec69f9c465d4a16586ca4befb53636c750b6ce6
Giulia-M <giuliamaggio16@gmail.com>
2021-04-13 23:54:39 +0200

    Controllo sul maiuscolo inputUtente?

--- myScript.js
+++ myScript.js
@@ -5,32 +5,34 @@ var inputUtente = prompt("Scrivi il tuo cognome");
 if (!inputUtente) {
     alert("Dato obbligatorio. Scrivi il tuo cognome")
 } else {
-    // array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+    // array con altri cognomi
     var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
-    //il cognome scritto dall'utente lo voglio inserire in un array con altri cognomi
+    //il cognome dell'utente lo voglio inserire nell'array con altri cognomi
     listaCognomi.push(inputUtente)
 
     // stampa la lista ordinata alfabeticamente
+    //CONTROLLO SUL MINUSCOLO -MAIUSCOLO? 
+     
+    /*var listaUpperCase = String(listaCognomi).toUpperCase().split(",");
+    var sorted = listaUpperCase.sort();
+    console.log(sorted);*/
+
     var sorted = listaCognomi.sort();
     console.log(sorted);
 
     // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
     var i = 0
-
-
     var containerList = document.getElementById("container_List")
-
-
     var posizioneUtente = -1;
+
     while (i < sorted.length) {
         var elementoDaAnalizzare = sorted[i]
-      
-       
+
         containerList.innerHTML += "<li>" + sorted[i] + "</li>";
 
         console.log(sorted[i])
-        
+
         // utilizzo l'IF per capire il posizionamento del cognome che scriverà l'utente 
         if (elementoDaAnalizzare === inputUtente) {
             posizioneUtente = i + 1;
